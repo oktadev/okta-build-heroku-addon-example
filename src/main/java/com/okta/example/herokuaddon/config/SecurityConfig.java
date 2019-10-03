e81e22ab0efa50b4f998123c6dd6b342d3e9cf09
Micah Silverman <micah@afitnerd.com>
2019-10-03 01:01:12 -0500

    Added basic auth to support heroku requests.

--- src/main/java/com/okta/example/herokuaddon/config/SecurityConfig.java
+++ src/main/java/com/okta/example/herokuaddon/config/SecurityConfig.java
@@ -1,18 +1,44 @@
 package com.okta.example.herokuaddon.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("#{ @environment['heroku.id'] }")
+    private String herokuId;
+
+    @Value("#{ @environment['heroku.password'] }")
+    private String herokuPassword;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser(herokuId).password(passwordEncoder().encode(herokuPassword))
+            .authorities("ROLE_USER");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
             .authorizeRequests()
-            .anyRequest()
-            .permitAll();
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
